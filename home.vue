271833fdc918604f55b696068f45145ca5e56f2e
Kristina Natalia <128212158+Kristina-natalia@users.noreply.github.com>
2023-10-06 21:18:41 +0800

    Create home.vue

---
+++ home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <MenuMakanan @tambah-makanan="tambahkanMakanan" />
+    <DaftarMakanan :pilihanMakanan="pilihanMakanan" @hapus-makanan="hapusMakanan" />
+    <HitungKalori :pilihanMakanan="pilihanMakanan" :kebutuhanKalori="kebutuhanKalori" />
+  </div>
+</template>
+
+<script>
+import MenuMakanan from '@/components/MenuMakanan.vue';
+import DaftarMakanan from '@/components/DaftarMakanan.vue';
+import HitungKalori from '@/components/HitungKalori.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    MenuMakanan,
+    DaftarMakanan,
+    HitungKalori,
+  },
+  data() {
+    return {
+      pilihanMakanan: [],
+      kebutuhanKalori: 2000, // Ganti dengan kebutuhan kalori yang sesuai
+    };
+  },
+  methods: {
+    tambahkanMakanan(makanan) {
+      this.pilihanMakanan.push(makanan);
+    },
+    hapusMakanan(index) {
+      this.pilihanMakanan.splice(index, 1);
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Gaya CSS sesuai keinginan Anda */
+</style>
