86f5ad1621bb3cca819867486c515325789b4913
Kristina Natalia <128212158+Kristina-natalia@users.noreply.github.com>
2023-10-06 21:13:36 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import VueRouter from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import Home from './views/Home.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   { path: '/', component: Home },
-]
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
 new Vue({
-  render: h => h(App),
-  router
-}).$mount('#app')
+  render: (h) => h(App),
+  router,
+}).$mount('#app');
