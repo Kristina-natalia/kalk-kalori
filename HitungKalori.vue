65a7ea818ce68aaa42619845970f133eb63cb57c
Kristina Natalia <128212158+Kristina-natalia@users.noreply.github.com>
2023-10-06 21:23:03 +0800

    Create HitungKalori.vue

---
+++ HitungKalori.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>Total Kalori</h2>
+    <p>Total Kalori: {{ totalKalori }}</p>
+    <p>Status: {{ statusKebutuhanKalori }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['pilihanMakanan', 'kebutuhanKalori'],
+  computed: {
+    totalKalori() {
+      return this.pilihanMakanan.reduce((total, makanan) => total + makanan.kalori, 0);
+    },
+    statusKebutuhanKalori() {
+      if (this.totalKalori >= this.kebutuhanKalori) {
+        return 'Cukup';
+      } else {
+        return 'Kurang';
+      }
+    },
+  },
+};
+</script>
