81d440d92c567fe96b76a3207a3d2a4bd50dc198
Kristina Natalia <128212158+Kristina-natalia@users.noreply.github.com>
2023-10-06 21:20:10 +0800

    Create MenuMakanan.vue

---
+++ MenuMakanan.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>Menu Makanan</h2>
+    <ul class="menu-list">
+      <li v-for="(makanan, index) in menuMakanan" :key="index" class="menu-item">
+        <div class="makanan-item">
+          <img :src="makanan.gambar" :alt="makanan.nama" class="makanan-gambar" @click="tambahkanMakanan(makanan)" />
+          <div class="makanan-detail">
+            <p>{{ makanan.nama }}</p>
+            <p>{{ makanan.kalori }} kalori</p>
+            <p>{{ makanan.deskripsi }}</p>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menuMakanan: [
+        { nama: 'Makanan 1', kalori: 200
